18783cb3cffc066de40d746bbd8fa930e946f315
JuliaSemeniuk <j.semeniuk.v@gmail.com>
2020-07-28 15:45:25 +0300

    start: reducer created

---
+++ src/Components/reducer.js
@@ -0,0 +1,14 @@
+import {} from './constants'
+
+const initialState = {
+    items: [],
+    img: '',
+    name: '',
+    price: '',
+},
+
+const Items = (store = initialState, action) => {
+
+}
+
+export default Items;
